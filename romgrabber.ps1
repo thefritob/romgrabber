3b41adf85ec7c0355d2433edc1908e25e856aa53
J Graham <thefritob@hotmail.com>
2023-11-11 18:17:23 -0800

    switched to the menu system.  Added folder cleanup

--- romgrabber.ps1
+++ romgrabber.ps1
@@ -1,9 +1,10 @@
 # Variables
-$URL = "https://www.example.com/files/system/gba/" # URL to fetch files from
-$keywords = @("\(us","us\)","usa\)") # Keywords to match, seperated by commas in quotes
-$excludeWords = @("beta", "pre-release","demo","bios","proto") # Words that filenames cannot contain
+$URL = "https://www.example.com/files/system/gg" # URL to fetch files from
+$downloadDirectory = "C:\\Downloads\\GG\\" # Directory to download files to
+$keywords = @("\(us","us\)","usa\)") # Keywords to match, seperated by commas in quotes, remeber to use escape characters"
+$excludeWords = @("\(beta", "\(pre-release","\(demo","\(bios","\(proto","\(alt","\[bios","demo ","\[b") # Words that filenames cannot contain
 $ignoreFiles = @("ignore1.txt", "ignore2.txt") # List of filenames to ignore
-$downloadDirectory = "C:\\Downloads\\GBA\\" # Directory to download files to
+$speedlimit = 2000 # Speed limit is the number of milliseconds a download must take or it will sleep that many before starting the next one
 
 # Get list of available files
 $webRequest = Invoke-WebRequest -Uri $URL
@@ -11,7 +12,7 @@ $webRequest = Invoke-WebRequest -Uri $URL
 # Filter links based on keywords, exclusion words, and ignore list
 $matchingLinks = $webRequest.Links | Where-Object {
     $link = $_
-    $fileName = Split-Path $link.href -Leaf
+    $fileName = Split-Path $link.innerText -Leaf
 
     # Check if any keyword matches (case-insensitive)
     $keywordMatch = $keywords | Where-Object { $fileName -imatch $_ }
@@ -30,37 +31,89 @@ $matchingLinks = $webRequest.Links | Where-Object {
 if (!(Test-Path -Path $downloadDirectory)) { New-Item -ItemType Directory -Path $downloadDirectory }
 
 # Group matching links by base filename up to the first parenthesis, case-insensitive)
-
 $groupedLinks = $matchingLinks | Group-Object -Property {$_ -ireplace '\(.*', '('}
 
 # For each group, select the link with the highest rev number (case-insensitive)
 $selectedLinks = foreach ($group in $groupedLinks) {$group.Group | Sort-Object -Property {$_.innerText} | Select-Object -First 1 }
 
-# Show summary and ask for confirmation
-$matchingCount = $selectedLinks.Count
-Write-Host "Found $matchingCount files to download."
-$userInput = Read-Host "Do you want to continue? (Y/N/List)"
-if ($userInput -eq "List") {
-    Write-Host "Files to download:"
-    foreach ($link in $selectedLinks) {
-        Write-Host $link.innerText
-    }
-    $userInput = Read-Host "Do you want to continue? (Y/N)"
-}
-if ($userInput -ne "Y") {
-    Write-Host "Download cancelled!"
-    return
+# Starting the menu system
+function Show-Menu
+{
+     param (
+           [string]$Title = 'Rom Grabber'
+     )
+     CLear-Host
+     Write-Host "================ $Title ================"
+     Write-Host "URL" - $URL
+     Write-Host "Download Directory" - $downloadDirectory
+     Write-Host "Files online matched" - $selectedLinks.Count
+
+     Write-Host "1: Press '1' to list files matched"
+     Write-Host "2: Press '2' to download missing files"
+     Write-Host "3: Press '3' cleanup download directory"
+     Write-Host "Q: Press 'Q' to quit."
 }
 
-# Download selected files
-foreach ($link in $selectedLinks) {
-    $fileUrl = $URL + $link.innerText
-    $fileName = Split-Path $link.href -Leaf
-    $goodname = Split-Path $link.innerText -Leaf
-    $filePath = Join-Path -Path $downloadDirectory -ChildPath $goodname
-    #This was for debuggin
-    #Write-Host "Saving" + $fileUrl "to" + $filePath
-    Invoke-WebRequest -Uri $fileUrl -OutFile $filePath
+# Do loop to make the menu system work
+do
+{
+     Show-Menu
+     $menuinput = Read-Host "Please make a selection"
+     switch ($menuinput)
+     {
+        '1' {
+            Clear-Host
+            Write-Host "Files to download:"
+            foreach ($link in $selectedLinks) {
+                Write-Host $link.innerText
+            }
+        } 
+        '2' {
+            Clear-Host
+            foreach ($link in $selectedLinks) {
+                $fileUrl = $URL + $link.innerText
+                # Using .href didnt' work because it did not like special characters
+                $fileName = Split-Path $link.href -Leaf
+                $goodname = Split-Path $link.innerText -Leaf
+                $filePath = Join-Path -Path $downloadDirectory -ChildPath $goodname
+                if(Test-Path -Path $filePath){Write-host "File" $filepath "exists skipping"}
+                else {
+                    # Log downloads to the screen with nicer name
+                    Write-Host "Saving" $fileUrl "to" $filePath
+                    $speed = Measure-Command { Invoke-WebRequest -Uri $fileUrl -OutFile $filePath -DisableKeepAlive} | Select-Object TotalMilliseconds
+                    if ($speed.TotalMilliseconds -lt $speedlimit){Start-Sleep -Milliseconds $speedlimit}
+                }
+            }
+        } 
+        '3' {
+            Clear-Host
+            Write-Host "Checking directory"
+            # Get all files in the directory
+            $directoryFiles = Get-ChildItem -Path $downloadDirectory -File | ForEach-Object { $_.Name }
+            # Make sure dir isn't empty
+            if ($directoryFiles -eq $null){
+                Write-Host "Download Directory Empty - skipping"
+                break  
+            }
+            # Compare to the online list
+            $otherfiles = Compare-object -ReferenceObject $selectedLinks.innerText -DifferenceObject $directoryFiles | Where-Object { $_.SideIndicator -eq "=>" } | ForEach-Object { $_.InputObject }
+            # List the files to move and ask for confirm
+            Write-Host "Files to move to the (other) folder:"
+            Write-Host $otherfiles
+            $userInput = Read-Host "Do you want to move these files? (Y/N)"
+            # Start the move
+            if ($userInput -eq "Y"){ 
+                # Create other directory if it doesn't exist
+                if (!(Test-Path -Path $downloadDirectory\\other)) {
+                    New-Item -ItemType Directory -Path $downloadDirectory\\other 
+                }
+                $otherfiles | ForEach-Object { Move-Item -Path $downloadDirectory\\$_ -Destination $downloadDirectory\\other}
+            }
+        }
+        'q' {
+            return
+        }    
+     }
+     pause
 }
-
-Write-Host "Download complete!"
+until ($input -eq 'q')
